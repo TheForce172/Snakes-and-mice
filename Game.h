0ee559cf2da5d77b5c5f25981060fa345b05c6b0
TaGenius <theforce172@gmail.com>
2018-11-27 11:20:30 +0000

    Refactor Game

---
+++ Game.h
@@ -0,0 +1,28 @@
+#ifndef GameH 
+#define GameH 
+
+#include "Mouse.h"
+#include"Snake.h"
+#include"UserInterface.h"
+#include <iostream>
+#include <sstream>
+class Game {
+public:
+	Mouse mouse_;
+	Snake snake_;
+	Underground underground_;
+	UserInterface* p_ui;
+
+	int key_;
+	void set_up(UserInterface* pui);
+	void run();
+	string prepare_grid();
+	bool is_arrow_key_code(int keycode);
+	void apply_rules();
+	int find_hole_number_at_position(int x, int y);
+	bool has_ended(char key);
+	string prepare_end_message();
+
+};
+
+#endif
