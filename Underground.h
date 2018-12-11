0cccfb606262660b88284670b291218f60d16ef4
TaGenius <theforce172@gmail.com>
2018-12-11 11:38:11 +0000

    nut fix

--- Underground.h
+++ Underground.h
@@ -15,7 +15,7 @@ class Underground
 	public:
 		Underground(vector<Hole>);
 		Hole get_hole_no(int no) const;
-		void set_hole_no_at_position(int no, int x, int y);
+		//void set_hole_no_at_position(int no, int x, int y);
 		size_t get_num_holes() const;
 	private:
 		bool is_valid_hole_number(int n) const;
