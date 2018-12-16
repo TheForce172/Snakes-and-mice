6d96249422d7b290ffd6b4e620ab194ddcbc7e0e
Arjan18 <b7020246@my.shu.ac.uk>
2018-12-16 14:33:38 +0000

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -12,7 +12,6 @@ class Snake : public MoveableGridItem{
 		void spot_mouse(Mouse* p_mouse);
 		void chase_mouse();
 		void reset();
-		void set_direction(int& dx, int& dy);
 		void moveTail(int snake_dx, int snake_dy);
 		char getTailSymbol();
 		bool gridAtTail(int row, int col);
